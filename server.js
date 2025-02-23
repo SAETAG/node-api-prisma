d044db03ac2beaa1275af7eef619bf78322f630c
SAETAG <saeko.96@gmail.com>
2025-02-23 04:56:40 +0000

    ファイル一式

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const express = require("express");
 // 2. ここで実行をします🤗appの箱の中でexpressの機能が使えるようにしています🤗
 const app = express();
 
+// CORS対策 npm i corsをしてインストールしてから記述🤗
+const cors = require("cors");
+
 // prismaのclientの機能を使えるようにする🤗
 const { PrismaClient } = require("@prisma/client");
 
@@ -24,8 +27,37 @@ const prisma = new PrismaClient();
 // 例: 8888の場合は localhost:8888になります🤗
 const PORT = 8888;
 
+// 必ずexpress.json()の上で記述！そうしないとcorsが回避できません！！
+app.use(cors());
+
 // jsで書いた文字列をjsonとしてexpressで使えるようにする必要があります🤗
 app.use(express.json());
+
+// ミドルウェア=レストランの入り口 で 「予約がしてる？」の確認のようなイメージ🤗
+// 予約（＝トークン）がある人 → 店内（＝APIの処理）に進める
+// 予約がない人 → 入れない（エラーになる）
+const authenticateToken = (req, res, next) => {
+  console.log("Authorization ヘッダー:", req.headers.authorization);
+
+  const token = req.headers.authorization?.split(" ")[1]; //予約（トークン）を取り出す
+  console.log("抽出されたトークン:", token);
+
+  if (!token) {
+    // 予約（トークン）がない場合は入れない
+    return res.status(401).json({ message: "認証トークンが必要です。" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.KEY); //予約（トークン）を確認
+    console.log("デコード結果:", decoded);
+
+    req.user = decoded; //OKならユーザー情報を設定🤗
+    next(); // 店（APIの処理）に進める
+  } catch (error) {
+    console.log("JWT 検証エラー:", error);
+    return res.status(403).json({ message: "無効なトークンです。" });
+  }
+};
 
 // 5.簡単なAPIの挙動を確認、作成してみます🤗
 // getはデータを表示するようなイメージです🤗
@@ -33,6 +65,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("<h1>おおほりは長野で研究しています!!</h1>");
 });
+
+
 
 //6.新規ユーザー登録のAPIを作成します🤗
 app.post("/api/auth/register", async (req, res) => {
@@ -94,12 +128,60 @@ app.post("/api/auth/login", async (req, res) => {
 });
 
 
+// 投稿API authenticateTokenは次のページで記述しています🤗
+app.post("/api/post", authenticateToken, async (req, res) => {
+  console.log("現在のユーザー ID:", req.user.id);
+  //userのidをチェックします🤗が流れとしては次のページを参考にしてください🤗
+
+  const { content } = req.body;
+  console.log(content, "content");
+  // contentが空の時=文字が入力されていないのでここでエラーでDBにデータを送らないようにする🤗
+  if (!content) {
+    return res.status(400).json({ message: "投稿内容が入力されていません" });
+  }
+
+  try {
+    // 成功した時にprismaを使用してデータを登録する🤗
+    const post = await prisma.post.create({
+      data: {
+        content,
+        authorId: req.user.id,
+      },
+      include: {
+        //ここはポイントになります！
+        author: true,
+      },
+    });
+    res.status(201).json(post);
+  } catch (err) {
+    console.log(err, "エラー内容");
+    res.status(500).json({ message: "サーバーエラーです。" });
+  }
+
+  // この下は消さない
+});
 
 
+// 投稿取得API🤗
+app.get("/api/posts", async (req, res) => {
+  try {
+    // 成功した時にprismaを使用してデータを取得🤗
+    const posts = await prisma.post.findMany({
+      take: 10, //これで最新の10件を取得できる🤗超便利！,
+      orderBy: { createdAt: "desc" }, //これで登録日から降順で取得(登録された最後のものから順番に取得)
+      include: {
+        //ここはポイントになります！
+        author: true,
+      },
+    });
+    res.status(201).json(posts);
+  } catch (err) {
+    console.log(err, "エラー内容");
+    res.status(500).json({ message: "サーバーエラーです。" });
+  }
 
-
-
-
+  // この下は消さない
+});
 
 
 // 絶対ここから上に書く（サーバー起動が最後！）
